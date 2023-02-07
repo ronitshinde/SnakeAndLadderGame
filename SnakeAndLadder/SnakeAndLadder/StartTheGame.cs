ef16f7fc82687601e0e3942088c2e06bdd89b314
ronitshinde <rony.shinde753@gmail.com>
2023-02-07 20:36:18 +0530

    [Ronit] Add : UC3-Player options to make move

--- SnakeAndLadder/SnakeAndLadder/StartTheGame.cs
+++ SnakeAndLadder/SnakeAndLadder/StartTheGame.cs
@@ -17,7 +17,34 @@ namespace SnakeAndLadder
             int dice_number = random.Next(1, 7);
             Console.WriteLine("Dice Number : " + dice_number);
             Console.ReadLine();
-                       
+
+            //UC-3
+            Random random1 = new Random();
+            int option = random1.Next(1, 4);
+            Console.WriteLine("Number of option " + option);
+            Console.Write("\n");
+            
+            int Player_position = 0;
+            int No_Play = 0;
+
+            if (option == 1)        //Moves ahead
+            {
+                Player_position = Player_position + dice_number;
+                Console.WriteLine("Player position moves ahead by the number on dice : " + Player_position);
+            }
+            else if (option == 2)   //Moves behind
+            {
+                Player_position = Player_position - dice_number;
+                Console.WriteLine("Player position moves behind by the number on dice : " + Player_position);
+            }
+            else                    //No play
+            {
+                Player_position = Player_position + No_Play;
+                Console.WriteLine("Player position remain same : " + Player_position);
+            }
+            Console.ReadLine();
         }
     }
 }
+                       
+        
