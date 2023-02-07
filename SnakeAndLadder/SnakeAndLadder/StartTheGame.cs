ef1903ed9ca687043a6333ceb9647b3a53b3553f
ronitshinde <rony.shinde753@gmail.com>
2023-02-07 21:12:19 +0530

    [Ronit] Add : UC4-To reach upto winning position

--- SnakeAndLadder/SnakeAndLadder/StartTheGame.cs
+++ SnakeAndLadder/SnakeAndLadder/StartTheGame.cs
@@ -6,45 +6,44 @@ namespace SnakeAndLadder
     {
         public void PlayerOne()
         {
-            //UC-1
-            int player_no = 1;
-            int start_position = 0;
-            Console.WriteLine("Start Position of Player no {0} is : {1} ", player_no, start_position);
-            Console.ReadLine();
-
-            //UC-2
-            Random random = new Random();
-            int dice_number = random.Next(1, 7);
-            Console.WriteLine("Dice Number : " + dice_number);
-            Console.ReadLine();
-
-            //UC-3
-            Random random1 = new Random();
-            int option = random1.Next(1, 4);
-            Console.WriteLine("Number of option " + option);
-            Console.Write("\n");
-            
+            //UC-4
             int Player_position = 0;
             int No_Play = 0;
+            while (Player_position <= 100)
 
-            if (option == 1)        //Moves ahead
             {
-                Player_position = Player_position + dice_number;
-                Console.WriteLine("Player position moves ahead by the number on dice : " + Player_position);
-            }
-            else if (option == 2)   //Moves behind
-            {
-                Player_position = Player_position - dice_number;
-                Console.WriteLine("Player position moves behind by the number on dice : " + Player_position);
-            }
-            else                    //No play
-            {
-                Player_position = Player_position + No_Play;
-                Console.WriteLine("Player position remain same : " + Player_position);
+                Random random = new Random();
+                int dice_number = random.Next(1, 7);
+                Console.WriteLine("Number on dice : " + dice_number);
+
+                Random random1 = new Random();
+                int option = random1.Next(1, 4);
+                Console.WriteLine("Number of option " + option);
+
+                if (option == 1)        //Moves ahead
+                {
+                    Player_position = Player_position + dice_number;
+                    Console.WriteLine("Player position moves ahead by {0} : {1}", dice_number, Player_position);
+                }
+
+                else if (option == 2)       //Moves behind
+                {
+                    Player_position = Player_position - dice_number;
+
+                    if (Player_position < 0)
+                    {
+                        Player_position = 0;
+                        Console.WriteLine("Player starts again from 0 position");
+                    }
+                    Console.WriteLine("Player position moves behind by {0} : {1}", dice_number, Player_position);
+                }
+                else                   //No play
+                {
+                    Player_position = Player_position + No_Play;
+                    Console.WriteLine("Player position remain same : " + Player_position);
+                }
             }
             Console.ReadLine();
         }
     }
 }
-                       
-        
