134a27544c8fbe62000eebcd1a7843add68f24ba
ronitshinde <rony.shinde753@gmail.com>
2023-02-07 20:17:29 +0530

    [Ronit] Add : UC2-Getting random dice number

--- SnakeAndLadder/SnakeAndLadder/StartTheGame.cs
+++ SnakeAndLadder/SnakeAndLadder/StartTheGame.cs
@@ -6,10 +6,18 @@ namespace SnakeAndLadder
     {
         public void PlayerOne()
         {
+            //UC-1
             int player_no = 1;
             int start_position = 0;
             Console.WriteLine("Start Position of Player no {0} is : {1} ", player_no, start_position);
             Console.ReadLine();
+
+            //UC-2
+            Random random = new Random();
+            int dice_number = random.Next(1, 7);
+            Console.WriteLine("Dice Number : " + dice_number);
+            Console.ReadLine();
+                       
         }
     }
 }
