0472c526e1d42ef793b76a8662a7b3c47ac848f4
ronitshinde <rony.shinde753@gmail.com>
2023-02-07 20:10:23 +0530

    [Ronit] Add : UC1-Start Position of the game

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            StartTheGame position = new StartTheGame();
+            position.PlayerOne();
+        }
+    }
+}
---
+++ SnakeAndLadder/SnakeAndLadder/StartTheGame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    public class StartTheGame
+    {
+        public void PlayerOne()
+        {
+            int player_no = 1;
+            int start_position = 0;
+            Console.WriteLine("Start Position of Player no {0} is : {1} ", player_no, start_position);
+            Console.ReadLine();
+        }
+    }
+}
