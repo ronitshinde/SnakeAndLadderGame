87dc39b005a1077dbf26c23d144a888fffccca9b
Ronit Shinde <118701033+ronitshinde@users.noreply.github.com>
2023-02-10 14:28:28 +0530

    [Ronit] Add : UC6-Total Dies Played In The Game

--- SnakeAndLadder/SnakeAndLadder/StartTheGame.cs
+++ SnakeAndLadder/SnakeAndLadder/StartTheGame.cs
@@ -6,18 +6,18 @@ namespace SnakeAndLadder
     {
         public void PlayerOne()
         {
-            //UC-5
-            int Player_position = 0;
-            int No_Play = 0;
-            int dieCount = 0;
-            while (Player_position <= 100)
+            //UC-6
+            int player_position = 0;
+            int no_Play = 0;
+            int die_Count = 0;
+            while (player_position <= 100)
 
             {
                 //Get dice number
                 Random random = new Random();
                 int dice_number = random.Next(1, 7);
                 Console.WriteLine("Number on dice : " + dice_number);
-                dieCount++;
+                die_Count++;
 
                 //Get the move to play
                 Random random1 = new Random();
@@ -26,41 +26,41 @@ namespace SnakeAndLadder
               
                 if (option == 1)        //Moves ahead using Ladder
                 {
-                    Player_position = Player_position + dice_number;
+                    player_position = player_position + dice_number;
 
-                    if (Player_position == 100)
+                    if (player_position == 100)
                     {
-                        Console.WriteLine("Player position is {0} and \" Player has won \" ", Player_position);
+                        Console.WriteLine("Player position is {0} and \" Player has won \" ", player_position);
                         break;
                     }
-                    else if (Player_position > 100)
+                    else if (player_position > 100)
                     {
-                        Player_position = Player_position - dice_number;
+                        player_position = player_position - dice_number;
                     }
 
-                    Console.WriteLine("Player position moves ahead by {0} : {1}", dice_number, Player_position);
+                    Console.WriteLine("Player position moves ahead by {0} : {1}", dice_number, player_position);
                 }
 
                 else if (option == 2)       //Moves behind using Snake
                 {
-                    Player_position = Player_position - dice_number;
+                    player_position = player_position - dice_number;
 
-                    if (Player_position < 0)
+                    if (player_position < 0)
                     {
-                        Player_position = 0;
+                        player_position = 0;
                         Console.WriteLine("Player starts again from 0 position");
                     }
 
-                    Console.WriteLine("Player position moves behind by {0} : {1}", dice_number, Player_position);
+                    Console.WriteLine("Player position moves behind by {0} : {1}", dice_number, player_position);
                 }
                 
                 else                   //No play
                 {
-                    Player_position = Player_position + No_Play;
-                    Console.WriteLine("Player position remain same : " + Player_position);
+                    player_position = player_position + no_Play;
+                    Console.WriteLine("Player position remain same : " + player_position);
                 }
             }
-            Console.WriteLine("Total dies played in the game : " + dieCount);
+            Console.WriteLine("Total dies played in the game : " + die_Count);
             Console.ReadLine();
         }
     }
